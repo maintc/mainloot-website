3312d3a3a612ccee8ef0d9e7e6d07dad040cfc17
cvega <casey.vega@gmail.com>
2024-08-25 22:37:36 -0700

    add servers api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()]
+  integrations: [react(), tailwind(), icon()],
+  output: "server",
+  adapter: cloudflare({
+    routes: {
+      extend: {
+        exclude: [{ pattern: '/api/*' }], // Use Starlight's pagefind search, which ise
+      }
+    }
+  })
 });
